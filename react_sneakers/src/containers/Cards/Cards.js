a066ae50e3329288c1632c213287717ff948be4c
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2023-12-02 18:00:46 +0700

    added backend api and behavering of plus button and farovite button

--- react_sneakers/src/containers/Cards/Cards.js
+++ react_sneakers/src/containers/Cards/Cards.js
@@ -1,8 +1,13 @@
 import Card from '../../components/Card/Card';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const CardsContainer = (props) => {
- const [cards, setCards] = useState(props.data)
+ const [cards, setCards] = useState([])
+
+ useEffect(() => {
+  setCards(props.data)
+ }, [props.data]);
+
  return (
   <>{cards.map((item, index) => {
    return <Card key={index} data={item} />
