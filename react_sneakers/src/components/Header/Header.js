93b17e056ea12ff145b3865765532dfabd2dde4a
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2024-01-16 21:09:20 +0700

    creacte main root font size var/ made adaptive and respons page-proof of header block

--- react_sneakers/src/components/Header/Header.js
+++ react_sneakers/src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = ({ openCart }) => {
   <header className={styles.header}>
    <div className="d-flex justify-between">
     <div className={styles.headerLeft}>
-     <img width={40} height={40} src='/img/logo.svg' alt='' className="mr-15" />
+     <img src='/img/logo.svg' alt='' className="mr-15" />
      <div className="header-info">
       <h3 className="text-uppercase">React Sneakers</h3>
       <p className='color-grey'>Магазин лучших кроссовок</p>
@@ -14,13 +14,13 @@ const Header = ({ openCart }) => {
     </div>
     <ul className={styles.headerRight}>
      <li className={styles.cartOpen} onClick={openCart} >
-      <img width={18} height={18} src='/img/cart.svg' alt='' className='mr-10' />
+      <img src='/img/cart.svg' alt='' className='header-icon mr-10' />
       <span>Корзина</span></li>
      <li className="d-flex align-center mr-20">
-      <Link to="/favorite" className='d-flex align-center' ><img className='mr-10' width={18} height={18} src='/img/favorite_heart.svg' alt='' /><span>Избранное</span></Link>
+      <Link to="/favorite" className='d-flex align-center' ><img className='header-icon mr-10' src='/img/favorite_heart.svg' alt='' /><span>Избранное</span></Link>
      </li>
      <li className="d-flex align-center">
-      <Link to="/profile" className='d-flex align-center' ><img className='mr-10' width={18} height={18} src='/img/user_icon.svg' alt='' /><span>Профиль</span></Link>
+      <Link to="/profile" className='d-flex align-center' ><img className='header-icon mr-10' src='/img/user_icon.svg' alt='' /><span>Профиль</span></Link>
 
      </li>
     </ul>
